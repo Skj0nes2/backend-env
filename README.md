f17c33d836286e98f7e89b76a027f9009c089d8b
Skj0nes2 <aric.jones@student.msdmartinsville.org>
2024-12-20 23:43:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# backend-env
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Skj0nes2/backend-env)
---
+++ test.js
@@ -0,0 +1,10 @@
+let isOnline = true;
+let isAdmin = true;
+
+if (isOnline != true) {
+  console.log(`Not Online`);
+} else if (isAdmin != true) {
+  console.log(`Not Online`);
+} else {
+  console.log(`Accepted!!!`);
+}
